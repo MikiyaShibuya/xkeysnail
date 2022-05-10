db6a25a8e9af5e4c8396dd617db721989b821c23
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2022-05-10 11:03:04 +0900

    Switch to jp with rihgt command, to en with left command

--- config.py
+++ config.py
@@ -5,20 +5,22 @@ from xkeysnail.transform import *
 
 # [Global modemap] Change modifier keys as in xmodmap
 define_modmap({
-    Key.RIGHT_ALT: Key.HENKAN,
-    Key.LEFT_META: Key.RIGHT_ALT,
+    Key.RIGHT_ALT: Key.HIRAGANA,
+    Key.RIGHT_META: Key.LEFT_META,
+    # Key.LEFT_META: Key.RIGHT_ALT,
+    Key.LEFT_META: Key.RIGHT_META,
 })
 
 define_multipurpose_modmap({
     Key.LEFT_ALT: [Key.HENKAN, Key.LEFT_CTRL],
-    # Key.LEFT_META: [Key.LEFT_CTRL, Key.LEFT_META]
+    # Key.RIGHT_META: [Key.LEFT_CTRL, Key.LEFT_META]
 })
 
 define_keymap(None, {
-    K('Alt-h'): K('LEFT'),
-    K('Alt-j'): K('DOWN'),
-    K('Alt-k'): K('UP'),
-    K('Alt-l'): K('RIGHT'),
+    K('RSuper-h'): K('LEFT'),
+    K('RSuper-j'): K('DOWN'),
+    K('RSuper-k'): K('UP'),
+    K('RSuper-l'): K('RIGHT'),
     K('Shift-ESC'): K('Shift-GRAVE'),
 })
 
