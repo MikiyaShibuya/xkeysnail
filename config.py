42c7d8df36814cabce31219707d07d42eb282c65
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2022-05-23 09:35:41 +0900

    Change using switch 5 to OFF

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ from xkeysnail.transform import *
 
 # [Global modemap] Change modifier keys as in xmodmap
 define_modmap({
-    Key.RIGHT_ALT: Key.HIRAGANA,
-    Key.RIGHT_META: Key.LEFT_META,
-    # Key.LEFT_META: Key.RIGHT_ALT,
-    Key.LEFT_META: Key.RIGHT_META,
+    Key.LEFT_META: Key.MUHENKAN,
+    Key.LEFT_ALT: Key.RIGHT_META,
+    Key.RIGHT_META: Key.HENKAN,
+    Key.RIGHT_ALT: Key.LEFT_META,
 })
 
 define_multipurpose_modmap({
-    Key.LEFT_ALT: [Key.HENKAN, Key.LEFT_CTRL],
-    # Key.RIGHT_META: [Key.LEFT_CTRL, Key.LEFT_META]
+    Key.MUHENKAN: [Key.MUHENKAN, Key.LEFT_CTRL],
+    Key.HENKAN: [Key.HENKAN, Key.LEFT_CTRL],
 })
 
 define_keymap(None, {
@@ -21,6 +21,10 @@ define_keymap(None, {
     K('RSuper-j'): K('DOWN'),
     K('RSuper-k'): K('UP'),
     K('RSuper-l'): K('RIGHT'),
+    K('Shift-RSuper-h'): K('Shift-LEFT'),
+    K('Shift-RSuper-j'): K('Shift-DOWN'),
+    K('Shift-RSuper-k'): K('Shift-UP'),
+    K('Shift-RSuper-l'): K('Shift-RIGHT'),
     K('Shift-ESC'): K('Shift-GRAVE'),
 })
 
